442dd56c38bf571191df28bedd1770e9d87412d1
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-03-07 18:45:03 +0100

    Adding console.log debug

--- Models/itemAcces.js
+++ Models/itemAcces.js
@@ -7,8 +7,10 @@ const myPool = require("./dbPool");
 async function getItems(){
   try{
     const items= await myPool.query("SELECT * FROM products");
+    console.log(items)
     return(items)
   } catch (err){
+    console.log("Error")
     return err;
   }
   
