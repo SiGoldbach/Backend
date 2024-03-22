8bc36105be2348126ede0f3115a5f51805ae5792
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-03-22 09:39:07 +0100

    Removeed unnused db commands

--- view.sql
+++ view.sql
@@ -14,37 +14,7 @@ LEFT JOIN discount d USING (product_id)
 LEFT JOIN upsell u USING (product_id)
 LEFT JOIN images i USING (product_id);
 
-SELECT 
-    p.product_id,
-    p.name, 
-    p.price,
-    p.currency
-    FROM
-    products p
-    INNER JOIN discount d USING (product_id);
-
-SELECT 
-    p.product_id,
-    p.name, 
-    p.price,
-    p.currency,
-    d.discount_amount,
-    d.discount_percent,
-    u.upsell_id
-FROM
-    products p
-INNER JOIN discount d USING (product_id)
-INNER JOIN upsell u USING (product_id);
-
-SELECT 
-    p.product_id,
-    p.name, 
-    p.price,
-    p.currency,
-    u.upsell_id
-FROM
-    products p
-LEFT JOIN upsell u USING (product_id);
+r
 
 
 
