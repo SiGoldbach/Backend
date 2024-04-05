a00c5a72b995e7de556beeb669f284322465f16d
Thorium <thor@iwanoff.dk>
2024-04-05 15:58:24 +0200

    Finally fixed postOrder

--- Models/orderAccess.js
+++ Models/orderAccess.js
@@ -1,13 +1,13 @@
 const { Client } = require('pg');
 const myPool = require("./dbPool");
 
-var jsonbody = require("./test.json")
-postOrder(jsonbody)
+const jsontest = require("./test.json")
+postOrder(jsontest)
 
-async function postOrder(body){
-    //try{
-        const order = require("./test.json")
-        console.log(order)
+async function postOrder(order){
+    try{
+        //const order = require("./test.json")
+        //console.log(order)
 
         const orderInfo = order.OrderInfo
         const order_return = await myPool.query("INSERT INTO orders VALUES(DEFAULT, $1,$2,$3,$4,$5,DEFAULT,DEFAULT,$6) RETURNING order_id",[
@@ -19,9 +19,7 @@ async function postOrder(body){
             orderInfo.comment
         ])
         const order_id = order_return.rows[0].order_id
-    console.log(typeof order_id)
-        console.log("ID: ")
-        console.log(order_id)
+
         for (let index = 0; index < order.OrderItems.length; index++) {
             const orderItem = order.OrderItems[index]
             const post = await myPool.query("INSERT INTO orderitems VALUES($1,$2,$3,$4,$5)",[
@@ -31,12 +29,11 @@ async function postOrder(body){
                 orderItem.currency,
                 orderItem.price
             ])
-
         }
 
-    //} catch(err) {
-    //    throw new Error(err);
-    //}
+    } catch(err) {
+        throw new Error(err);
+    }
 }
 
 
