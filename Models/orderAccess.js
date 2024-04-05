c1c80d25e25b885c50e48ff57eb81230cb3a7ee2
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2024-04-05 11:26:56 +0200

    early write-up of postOrder and creation of test folder for a testing the JSON formatting

--- Models/orderAccess.js
+++ Models/orderAccess.js
@@ -6,6 +6,27 @@ const myPool = require("./dbPool");
 
 
 
+async function postOrder(body){
+    try{
+      const post = await myPool.query("INSERT INTO products VALUES(DEFAULT, $1,$2,$3,$4,$5)",[body.email,body.name,body.address,body.bill_address,body.comment])
+  
+      for (let index = 1; index < array.length; index++) {
+        
+        const post = await myPool.query("INSERT INTO products VALUES(DEFAULT, $1,$2,$3,$5)",[body.product_id,body.quantity,body.price,body.currency])
+        const element = array[index];
+      }); {
+        
+
+
+
+    }
+    catch(err){
+      throw new Error(err);
+  
+    }
+  
+  }
+
 
 
 
