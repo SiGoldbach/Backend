3f8cdecd7a1c2a3a4d4018654ffe01451249f737
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-04-12 11:13:13 +0200

    I am trying to serve a static fil on /

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, '../../FrontendWebShop9/dist')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/items', itemRoutes)
