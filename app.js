cd13855291d821e874513792a010d4fa04618e55
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-04-12 11:23:49 +0200

    Now i should have the correct path

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, '../../FrontendWebShop9/dist')));
+app.use(express.static(path.join(__dirname, '../FrontendWebShop9/dist/')));
 
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
