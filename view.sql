78c1afd7a4bd6fa911d8114e5b417d97dceb86f7
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-04-18 11:14:13 +0200

    Now there is a transaction in the sql file to make an order

--- view.sql
+++ view.sql
@@ -15,6 +15,31 @@ LEFT JOIN upsell u USING (product_id)
 LEFT JOIN images i USING (product_id);
 
 
+CREATE OR REPLACE FUNCTION submitOrder(email TEXT, _name TEXT, adress TEXT, bill_adress text, tlf TEXT, comment TEXT, productIDS INTEGER[], quantity INTEGER[], currency TEXT[], price REAL[], productAmount INTEGER)
+RETURNS VOID AS $$
+DECLARE 
+    orderID INT;
+BEGIN 
+    BEGIN
+        INSERT INTO orders VALUES(DEFAULT,email,_name,adress,bill_adress,tlf,0,DEFAULT,comment)
+        RETURNING order_id into orderID;
+
+        FOR i in 1..productAmount LOOP
+            INSERT INTO orderitems VALUES(orderID,productIDS[i],quantity[i],currency[i],price[i]);
+
+        END LOOP;
+
+        EXCEPTION
+        -- If any error occurs, rollback the transaction
+        WHEN OTHERS THEN
+            RAISE NOTICE 'Error: %', SQLERRM;
+            ROLLBACK;
+    END;
+END; 
+$$ LANGUAGE plpgsql;
+
+
+
 
 
 
