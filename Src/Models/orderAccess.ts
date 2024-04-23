fb394c39bedd302143fa53e6cec1c0f7f817231b
SiGoldbach <91067502+SiGoldbach@users.noreply.github.com>
2024-04-23 11:22:39 +0200

    Updating syntax of insert order

--- Src/Models/orderAccess.ts
+++ Src/Models/orderAccess.ts
@@ -17,16 +17,17 @@ export async function postOrder(order: Order, orderItems: OrderItems,user: User)
         await client.connect();
         await client.query('BEGIN');
         const order_id: number= (await client.query('INSERT INTO orders VALUES (DEFAULT,$1,$2,DEFAULT,DEFAULT,$3,$4,$5,$6) RETURNING order_id ',[order.email,order.tlf,order.name,order.adress,order.adress,order.comment])).rows[0].order_id;
-        const userAlreadyExists: boolean = (await client.query('SELECT EXISTS (SELECT 1 FROM orders WHERE email=$1',[order.email])).rows[0].exists;
+        const userAlreadyExists: boolean = (await client.query('SELECT EXISTS (SELECT 1 FROM users WHERE email=$1)',[order.email])).rows[0].exists;
         if(userAlreadyExists){
           await client.query('UPDATE users SET marketing = $1 WHERE email = $2',[user.marketing,user.email])
         }else{
-          await client.query('INSERT INTO users VALUES($1,$2) ',[order.email])
+          await client.query('INSERT INTO users VALUES ($1,$2) ',[order.email])
         }
         await client.query('COMMIT');
+        console.log("order has been commited");
       }catch(error: any){
         await client.query('ROLLBACK');
-        console.error('Error:', error.message);
+        console.error('Error in transaction rollback succesfull:', error.message);
       }finally{
         await client.end();
 
