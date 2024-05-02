a2841532b93a4e33f3e6dd0d54631d56c3b093f7
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2024-05-02 12:30:04 +0200

    fixed errors

--- DB_DockerConf/init-db.sql
+++ DB_DockerConf/init-db.sql
@@ -1,5 +1,4 @@
 
-
 CREATE TABLE products (
 product_id SERIAL PRIMARY KEY,
 name varchar(64) NOT NULL,
@@ -9,37 +8,22 @@ currency varchar(12) NOT NULL
 );
 
 CREATE TABLE discount(
-discount_id 
-SERIAL PRIMARY KEY,
-product_id int 
-NOT NULL 
-REFERENCES products (product_id) 
-ON DELETE CASCADE,
-discount_amount int 
-NOT NULL,  -- buy at least this many to get discount
-discount_percent float(4) 
-NOT NULL,
+discount_id SERIAL PRIMARY KEY,
+product_id int NOT NULL REFERENCES products (product_id) ON DELETE CASCADE,
+discount_amount int NOT NULL,  -- buy at least this many to get discount
+discount_percent float(4) NOT NULL
 );
 
 CREATE TABLE upsell(
-product_id int  
-PRIMARY KEY 
-REFERENCES products (product_id) 
-ON DELETE CASCADE,
-upsell_id int 
-PRIMARY KEY 
-REFERENCES products (product_id) 
-ON DELETE CASCADE
+product_id int REFERENCES products (product_id) ON DELETE CASCADE,
+upsell_id int REFERENCES products (product_id) ON DELETE CASCADE,
+PRIMARY KEY (product_id, upsell_id)
 );
 ALTER TABLE upsell ADD CONSTRAINT upsell_same_id CHECK (product_id != upsell_id);
 
 CREATE TABLE inventory(
-product_id int 
-PRIMARY KEY 
-REFERENCES products (product_id) 
-ON DELETE CASCADE,
-stock int 
-NOT NULL
+product_id int PRIMARY KEY REFERENCES products (product_id) ON DELETE CASCADE,
+stock int NOT NULL
 );
 
 CREATE TABLE orders(
@@ -51,32 +35,21 @@ address varchar(255) NOT NULL,
 bill_address varchar(255) NOT NULL,
 comment varchar(255),
 processed BOOLEAN NOT NULL DEFAULT 0,
-date BOOLEAN NOT NULL DEFAULT GETDATE()
+date date NOT NULL DEFAULT GETDATE()
 );
- 
+
 CREATE TABLE orderitems(
-order_id int  
-PRIMARY KEY 
-REFERENCES orders (order_id) 
-ON DELETE CASCADE,
-product_id int  
-PRIMARY KEY 
-REFERENCES products (product_id) 
-ON DELETE CASCADE,
-quantity int 
-NOT NULL,
-price float(16) 
-NOT NULL,
-currency varchar(12) NOT NULL
+order_id int REFERENCES orders (order_id) ON DELETE CASCADE,
+product_id int REFERENCES products (product_id) ON DELETE CASCADE,
+quantity int NOT NULL,
+price float(16) NOT NULL,
+currency varchar(12) NOT NULL,
+PRIMARY KEY (order_id, product_id)
 );
 
 CREATE TABLE images(
-product_id int 
-PRIMARY KEY 
-REFERENCES products (product_id) 
-ON DELETE CASCADE,
-image_url varchar(255) 
-NOT NULL
+product_id int PRIMARY KEY REFERENCES products (product_id) ON DELETE CASCADE,
+image_url varchar(255) NOT NULL
 );
 
 CREATE TABLE users(
